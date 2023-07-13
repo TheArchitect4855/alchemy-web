18fdba162b6e6cc5080ec4f7449f6059c2487781
Kurtis Knodel <kurtis@kurtisknodel.com>
2023-07-12 18:42:46 -0700

    Removed dark theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,73 +1,60 @@
 <svelte:head>
-	<title>Alchemy</title>
+  <title>Alchemy</title>
 </svelte:head>
 
-<slot></slot>
+<slot />
 
 <style>
-	@media (prefers-color-scheme: dark) {
-		:global(:root) {
-			--background-color: #222;
-			--text-color: #fff;
-			--subtitle-color: #ccc;
-			--primary-color: #7cff66;
-			--secondary-color: #00d2fc;
-			--error-color: #f50000;
-		}
-	}
+  :global(:root) {
+    --background-color: #fff;
+    --text-color: #222;
+    --subtitle-color: #555;
+    --primary-color: #7cff66;
+    --secondary-color: #00d2fc;
+    --error-color: #f50000;
+  }
 
-	@media (prefers-color-scheme: light) {
-		:global(:root) {
-			--background-color: #fff;
-			--text-color: #222;
-			--subtitle-color: #555;
-			--primary-color: #7cff66;
-			--secondary-color: #00d2fc;
-			--error-color: #f50000;
-		}
-	}
+  :global(a) {
+    color: var(--secondary-color);
+    text-decoration: none;
+  }
 
-	:global(a) {
-		color: var(--secondary-color);
-		text-decoration: none;
-	}
+  :global(body) {
+    background-color: var(--background-color);
+    color: var(--text-color);
+    font-family: "Lexend", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
 
-	:global(body) {
-		background-color: var(--background-color);
-		color: var(--text-color);
-		font-family: 'Lexend', sans-serif;
-		margin: 0;
-		padding: 0;
-	}
+  :global(button) {
+    background-color: var(--primary-color);
+    border: none;
+    border-radius: 16px;
+    font-family: "Lexend", sans-serif;
+    font-weight: bold;
+    padding: 8px 24px;
+  }
 
-	:global(button) {
-		background-color: var(--primary-color);
-		border: none;
-		border-radius: 16px;
-		font-family: 'Lexend', sans-serif;
-		font-weight: bold;
-		padding: 8px 24px;
-	}
+  :global(dialog) {
+    border: none;
+    border-radius: 16px;
+    padding: 16px;
+  }
 
-	:global(dialog) {
-		border: none;
-		border-radius: 16px;
-		padding: 16px;
-	}
+  :global(dialog::backdrop) {
+    backdrop-filter: blur(4px);
+  }
 
-	:global(dialog::backdrop) {
-		backdrop-filter: blur(4px);
-	}
+  :global(h1, h2, h3) {
+    color: var(--text-color);
+  }
 
-	:global(h1, h2, h3) {
-		color: var(--text-color);
-	}
+  :global(h4, h5, h6) {
+    color: var(--subtitle-color);
+  }
 
-	:global(h4, h5, h6) {
-		color: var(--subtitle-color);
-	}
-
-	:global(main) {
-		padding: 16px;
-	}
+  :global(main) {
+    padding: 16px;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,116 +1,111 @@
 <script>
-	import Footer from "./Footer.svelte";
-	import HeroIcon from "./HeroIcon.svelte";
+  import Footer from "./Footer.svelte";
+  import HeroIcon from "./HeroIcon.svelte";
 </script>
 
 <div class="container">
-	<div class="mobile" style="height: 20vh; height: 20lvh; scroll-snap-align: start;"></div>
-	<div class="wrapper">
-		<main>
-			<div class="content">
-				<HeroIcon />
-				<h1>Welcome</h1>
-				<h4>
-					Alchemy is the new dating app, made specifically for neurodiverse people.
-				</h4>
+  <div
+    class="mobile"
+    style="height: 20vh; height: 20lvh; scroll-snap-align: start;"
+  />
+  <div class="wrapper">
+    <main>
+      <div class="content">
+        <HeroIcon />
+        <h1>Welcome</h1>
+        <h4>
+          Alchemy is the new dating app, made specifically for neurodiverse
+          people.
+        </h4>
 
-				<p>
-					Neurodiverse people <i>date differently.</i> In most cases, this makes
-					finding "the one" much more difficult simply because of our different
-					approach. <br /> Alchemy aims to fix this: It creates a safe and accepting
-					environment for neurodiverse individuals to date like-minded people
-					who understand the things that make them different.
-				</p>
+        <p>
+          Neurodiverse people <i>date differently.</i> In most cases, this makes
+          finding "the one" much more difficult simply because of our different
+          approach. <br /> Alchemy aims to fix this: It creates a safe and accepting
+          environment for neurodiverse individuals to date like-minded people who
+          understand the things that make them different.
+        </p>
 
-				<p>
-					You don't have to be neurodiverse to use Alchemy - You just have
-					to be understanding and supportive of the struggles neurodiverse
-					people face in dating.
-				</p>
+        <p>
+          You don't have to be neurodiverse to use Alchemy - You just have to be
+          understanding and supportive of the struggles neurodiverse people face
+          in dating.
+        </p>
 
-				<h3>Interested?</h3>
-				<span>Coming soon to Android and iOS.</span>
-			</div>
-		</main>
-	</div>
-	<Footer />
+        <h3>Interested?</h3>
+        <span>Coming soon to Android and iOS.</span>
+      </div>
+    </main>
+  </div>
+  <Footer />
 </div>
 
 <style>
-	@media screen and (max-width: 500px) {
-		.container {
-			scroll-snap-type: y mandatory;
-		}
+  @media screen and (max-width: 500px) {
+    .container {
+      scroll-snap-type: y mandatory;
+    }
 
-		main {
-			/* fallbacks */
-			min-height: calc(100vh - 24px);
+    main {
+      /* fallbacks */
+      min-height: calc(100vh - 24px);
 
-			/* actual values */
-			border-top-left-radius: 16px;
-			border-top-right-radius: 16px;
-			min-height: calc(100lvh - 24px);
-			scroll-snap-align: start;
-		}
-	}
+      /* actual values */
+      border-top-left-radius: 16px;
+      border-top-right-radius: 16px;
+      min-height: calc(100lvh - 24px);
+      scroll-snap-align: start;
+    }
+  }
 
-	@media screen and (min-width: 500px) {
-		.mobile {
-			display: none;
-		}
+  @media screen and (min-width: 500px) {
+    .mobile {
+      display: none;
+    }
 
-		.wrapper {
-			min-height: 100vh;
-			min-height: 100lvh;
-		}
+    .wrapper {
+      min-height: 100vh;
+      min-height: 100lvh;
+    }
 
-		main {
-			aspect-ratio: 1.618;
-			border-radius: 16px;
-			margin: 10vh auto;
-			max-height: 80vh;
-			max-width: 80vw;
-		}
-	}
+    main {
+      aspect-ratio: 1.618;
+      border-radius: 16px;
+      margin: 10vh auto;
+      max-height: 80vh;
+      max-width: 80vw;
+    }
+  }
 
-	@media (prefers-color-scheme: dark) {
-		.container {
-			background-color: black;
-			background-image: url("/plants-bg-dark.png");
-		}
-	}
+  .container {
+    background-color: var(--primary-color);
+    background-image: url("/plants-bg-light.png");
+  }
 
-	@media (prefers-color-scheme: light) {
-		.container {
-			background-color: var(--primary-color);
-			background-image: url("/plants-bg-light.png");
-		}
-	}
+  .container {
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: repeat;
+    overflow-y: auto;
 
-	.container {
-		background-attachment: fixed;
-		background-position: center;
-		background-repeat: repeat;
-		overflow-y: auto;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
 
-		position: fixed;
-		left: 0;
-		right: 0;
-		top: 0;
-		bottom: 0;
-	}
+  .content {
+    margin: 0 auto;
+    max-width: 50ch;
+  }
 
-	.content {
-		margin: 0 auto;
-		max-width: 50ch;
-	}
+  main {
+    background-color: var(--background-color);
+    text-align: center;
+  }
 
-	main {
-		background-color: var(--background-color);
-		text-align: center;
-	}
-
-	main p {
-		text-align: left;
-	}
+  main p {
+    text-align: left;
+  }
 </style>
