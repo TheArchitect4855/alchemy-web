b912fff54770658a5dd8e22e68adf685db513075
Kurtis Knodel <kurtis@kurtisknodel.com>
2023-07-12 18:49:54 -0700

    Tweaked footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,36 +1,49 @@
 <footer>
-	<section>
-		<h4>Legal</h4>
-		<a href="/legal/tos">Terms of Service</a>
-		<a href="/legal/privacy">Privacy Policy</a>
-	</section>
+  <h2>
+    <a href="/">Alchemy</a>
+  </h2>
+  <div class="content">
+    <section>
+      <h4>Legal</h4>
+      <a href="/legal/tos">Terms of Service</a>
+      <a href="/legal/privacy">Privacy Policy</a>
+    </section>
+  </div>
 </footer>
 
 <style>
-	footer {
-		background-color: black;
-		color: white;
-		font-size: 0.8rem;
-		scroll-snap-align: start;
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		flex-wrap: wrap;
-	}
+  .content > section {
+    padding: 1rem;
+    text-align: center;
+  }
 
-	footer h4 {
-		color: white;
-		margin: 0;
-	}
+  .content > section > * {
+    display: block;
+  }
 
-	footer > section {
-		padding: 1rem;
-		text-align: center;
-	}
+  .content h4 {
+    color: white;
+    margin: 0;
+  }
 
-	footer > section > * {
-		display: block;
-	}
+  footer {
+    background-color: black;
+    color: white;
+    font-size: 0.8rem;
+    scroll-snap-align: start;
+  }
+
+  footer > h2 {
+    margin: 0;
+    padding: 1rem;
+    text-align: center;
+  }
 </style>
