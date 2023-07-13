cd96bd5ee0dd4eeafd0854610195a849f1b2bb79
Kurtis Knodel <kurtis@kurtisknodel.com>
2023-07-12 18:40:53 -0700

    Created privacy policy

--- src/routes/legal/privacy/+page.svelte
+++ src/routes/legal/privacy/+page.svelte
@@ -1,2 +1,185 @@
-<h1>Privacy Policy</h1>
+<h2>Our Commitment to You</h2>
+<p>
+  Alchemy cares about your privacy. All of the data Alchemy stores about you is
+  encrypted at rest, and it is not sold or otherwise disclosed without your
+  permission.
+</p>
 
+<h1>Privacy Policy</h1>
+<p>
+  This Policy intends to describe Alchemy's privacy practices in an
+  understandable manner with minimal legal and technical language.
+</p>
+<p>This Privacy Policy applies from July 11, 2023.</p>
+
+<h2>1. Who We Are.</h2>
+<p>
+  Alchemy <br />
+  769 Fordham Rd <br />
+  Kelowna, BC V1W 1P3 <br />
+  Canada
+</p>
+
+<h2>2. Where This Privacy Policy Applies.</h2>
+<p>
+  This Privacy Policy applies to websites, apps, events, and other services
+  Alchemy operates. For simplicity, these will be referred to as Alchemy's
+  "services" in this Privacy Policy.
+</p>
+
+<h2>3. Information We Collect.</h2>
+<p>
+  Since Alchemy is a dating app, we do need to collect information about you.
+  This includes basic profile details and dating interests. We also collect
+  location data in order to show you nearby potential matches. See below for
+  more details.
+</p>
+<h3>Information you give us.</h3>
+<p>
+  You choose to give Alchemy certain information when using our services. This
+  includes:
+</p>
+<ul>
+  <li>Your phone number</li>
+  <li>Your date of birth</li>
+  <li>Your name</li>
+  <li>Your GPS location</li>
+  <li>Photos to use on your profile</li>
+  <li>Dating preferences, such as your gender interests</li>
+</ul>
+<p>and optionally,</p>
+<ul>
+  <li>A bio</li>
+  <li>Your gender identity</li>
+  <li>Your interests</li>
+  <li>Any neurodiversities you may have</li>
+  <li>Your preferred pronouns</li>
+  <li>Whether or not you're transgender</li>
+  <li>Profiles you've liked</li>
+  <li>Chat messages you've sent</li>
+</ul>
+<p>
+  Sharing any of this information with Alchemy authorizes us to use it as
+  outlined in this Privacy Policy.
+</p>
+<h3>Information generated or automatically collected when you our services.</h3>
+<p>
+  Alchemy will also generate or collect data that is needed for our services to
+  work. This includes:
+</p>
+<ul>
+  <li>The name of your current locality (i.e. city name)</li>
+  <li>Log files generated on your device</li>
+</ul>
+
+<h2>4. How We Use Information.</h2>
+<p>
+  All information Alchemy collects about you is used for the purposes outlined
+  below.
+</p>
+<h3>I. To provide our services to you</h3>
+<p>
+  The information we collect is used to provide the Alchemy app. Your public
+  profile data (such as name, bio, etc.) is used to show other members who you
+  are so you can make connections with them.
+</p>
+<p>
+  Private data, such as your dating preferences, location, and whether or not
+  you're transgender, is used to help filter your and other member's potential
+  matches to people you're more likely to be interested in.
+</p>
+<h3>II. To improve our services</h3>
+<p>
+  Some data, such as log files, is used for debugging purposes or internal
+  research to improve our services. This is to help find bugs and issues with
+  the app, and to help determine which features users like or don't like.
+</p>
+<h3>
+  III. To prevent, detect, and fight fraud or other illegal or unauthorized
+  activities
+</h3>
+<p>
+  Data we collect about you can be used to help detect and deter malicious or
+  illegal activities of you or other members.
+</p>
+<h2>5. How We Share Information.</h2>
+<p>
+  Primarily, Alchemy shares your data with other members. This is to allow you
+  and other members to make connections with each other.
+</p>
+<p>
+  Alchemy also shares information with our service providers. In most cases, the
+  information shared with these service providers is stored securely and only
+  accessed by Alchemy. If this is not the case, the data shared with these
+  service providers is kept to an absolute minimum.
+</p>
+<p>
+  Alchemy will also share information if it is legally required to, the request
+  is reasonably necessary, and the request is about a specific member or
+  specific member(s) (that is, Alchemy will not respond to general requests for
+  information such as "which members of your platform are _____?").
+</p>
+
+<h2>6. Your Rights and Choices.</h2>
+<p>
+  You're in control of your information. Below is a list of the options and
+  tools available to you regarding your information:
+</p>
+<ul>
+  <li>
+    <b>Profile and account settings in the service.</b>
+    The edit profile and settings functions allow you to access, rectify, or remove
+    information that you provided to us and that's associated directly to your account
+    within the service.
+  </li>
+  <li>
+    <b>Device permissions.</b>
+    Mobile platforms can have permission systems for specific types of device data
+    and notifications, such as pictures, location services, push notifications, and
+    advertising identifiers. You can change your settings on your device to either
+    consent or oppose the collection or processing of the corresponding information
+    or the display of the corresponding notifications. Doing this may cause certain
+    services to lose functionality.
+  </li>
+  <li>
+    <b>Uninstall.</b>
+    You can stop all information collection by Alchemy by uninstalling the app. Remember
+    that uninstalling the app does not close your account! To close your account,
+    please use the corresponding functionality on the service.
+  </li>
+  <li>
+    <b>Account closure.</b>
+    You can close your account at any time by using the corresponding functionality
+    directly on the service.
+  </li>
+</ul>
+<p>
+  You also have the right to access, delete, update, and object to the
+  collection of your information. To do any of these, you can contact Alchemy by
+  email at <a href="mailto:privacy@usealchemy.app">privacy@usealchemy.app</a>.
+  <br />
+  Please note that this will also require proof of identity to protect the information
+  and privacy of other members.
+</p>
+
+<h2>7. No Children Allowed.</h2>
+<p>
+  Alchemy is restricted to individuals who are 18 years of age or older. We do
+  not permit individuals under the age of 18 on our platform. If you suspect
+  that a member is under the age of 18, please use the reporting mechanism
+  available on the service.
+</p>
+
+<h2>8. Privacy Policy Changes.</h2>
+<p>
+  This Privacy Policy may change over time. Alchemy may not notify you, and as
+  such we recommend you regularly check
+  <a href="https://usealchemy.app/legal/privacy">our website</a> for any changes.
+</p>
+
+<h2>9. How to Contact Us.</h2>
+<p>
+  If you have any questions about this Privacy Policy, please contact us by
+  email at
+  <a href="mailto:privacy@usealchemy.app">privacy@usealchemy.app</a>.
+</p>
